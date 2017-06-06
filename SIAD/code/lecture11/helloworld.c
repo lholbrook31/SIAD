6549118ad7990edc99f611266d20a7c5d34eb60b
Seed <seed@ubuntu.(none)>
2017-06-06 09:20:17 -0700

    Initial commit

--- SIAD/code/lecture11/helloworld.c
+++ SIAD/code/lecture11/helloworld.c
@@ -3,7 +3,13 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]){
+	char *data = getenv("QUERY_STRING");
+	char *username[100];
+	char *password[100];
+
 	printf("Content-Type: text/plain; charset=utf-8\n\n");
-	printf("Hello World\n");
+	printf("Hello World: %s\n", data);
+	sscanf(data, "username=%[^&]&password=%s", username, password);
+
 	return 0;
 }
